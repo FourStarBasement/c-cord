c8aad923da3c404a359e6c9ba2c8ea18b2a63482
Intexisty <legoguy253@gmail.com>
2020-06-13 18:32:58 -0500

    update cmake, remove libuv stuff

--- src/client.c
+++ src/client.c
@@ -45,56 +45,11 @@ void discord_client_run(discord_client* _discord_client) {
     }
 }
 
-void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
-    buf->base = malloc(suggested_size);
-    buf->len = suggested_size;
-}
+void discord_ws_response() {}
 
-void discord_ws_response(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
-    if (nread < 0) {
-        if (nread != UV_EOF)
-            fprintf(stderr, "Read error %s\n", uv_err_name(nread));
-        uv_close((uv_handle_t*) client, NULL);
-        free(buf->base);
-        free(client);
-        return;
-    }
+void discord_ws_connect() { }
 
-    char *data = (char*) malloc(sizeof(char) * (nread+1));
-    data[nread] = '\0';
-    strncpy(data, buf->base, nread);
-
-    fprintf(stderr, "%s", data);
-    free(data);
-    free(buf->base);
-}
-
-void discord_ws_connect(uv_connect_t* req, int status) {
-    if (status < 0) {
-        fprintf(stderr, "connect failed error %s\n", uv_err_name(status));
-        free(req);
-        return;
-    }
-
-    uv_read_start((uv_stream_t*) req->handle, alloc_buffer, discord_ws_response);
-    free(req);
-}
-
-void discord_ws_open(char* discord_wss_url, short port) {
-    /* setup websocket and loop for libuv */
-    uv_loop_t *loop = malloc(sizeof(uv_loop_t));
-    uv_loop_init(loop);
-    uv_tcp_t* socket = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
-    uv_tcp_init(loop, socket);
-
-    /* setup connection to discord websocket */
-    uv_connect_t* connect = (uv_connect_t*)malloc(sizeof(uv_connect_t));
-    struct sockaddr_in dest;
-    uv_ip4_addr(discord_wss_url, port, &dest);
-
-    uv_tcp_connect(connect, socket, (const struct sockaddr*)&dest, discord_ws_connect);
-    uv_run(loop, UV_RUN_DEFAULT);
-}
+void discord_ws_open(char* discord_wss_url, short port) { }
 
 void discord_ws_close() {
 
