f2234ee70afb571177ec88406e89ee9d1c28174d
John Wiggins <lilwiggy123@gmail.com>
2020-06-15 01:46:25 -0500

    Remove strcat()
    This causes the JSON to decode improperly as it received bufferoverflow errors

--- src/client.c
+++ src/client.c
@@ -3,51 +3,53 @@
 
 #include <string.h>
 #include <jansson.h>
+#include <libwebsockets.h>
 
 discord_client* global_discord_client;
 
 discord_config* discord_config_new(char* _token, short _token_type, int _message_cache_size) {
-    /* initialize a new discord_config */
-    discord_config *p = malloc(sizeof *p);
+	/* initialize a new discord_config */
+	discord_config* p = malloc(sizeof * p);
 
-    /* copy token over */
-    p->token = _token;
-    /* set token type */
-    p->token_type = _token_type;
-    /* set message cache size */
-    p->message_cache_size = _message_cache_size;
+	/* copy token over */
+	p->token = _token;
+	/* set token type */
+	p->token_type = _token_type;
+	/* set message cache size */
+	p->message_cache_size = _message_cache_size;
 
-    return p;
+	return p;
 }
 
 discord_client* discord_client_new(discord_config* _discord_config) {
-    /* initialize a new discord_client */
-    discord_client *p = malloc(sizeof *p);
-    p->discord_config = _discord_config;
-    return p;
+	/* initialize a new discord_client */
+	discord_client* p = malloc(sizeof * p);
+	p->discord_config = _discord_config;
+	return p;
 }
 
 void discord_client_run(discord_client* _discord_client) {
-    /* init global_discord_client */
-    global_discord_client = _discord_client;
-    /* set client running to true */
-    global_discord_client->run = true;
+	/* init global_discord_client */
+	global_discord_client = _discord_client;
+	/* set client running to true */
+	global_discord_client->run = true;
 
-    char* gateway_resp = discord_get(global_discord_client->discord_config->token_type == BOT ? strcat(DISCORD_URL, GATEWAY_BOT) : strcat(DISCORD_URL, GATEWAY), discord_headers());
-
-    /* parse json */
-    json_t* root;
-    json_error_t error;
-    root = json_loads(gateway_resp, 0, &error);
-    free(gateway_resp);
-    char* url = json_string_value(json_object_get(root, "url"));
-    if (url == NULL) {
-        printf("invalid token entered, exiting...");
-        exit(0);
-    } else {
-        /* pretty sure it's port 80, but whatever. */
-        discord_ws_open(url, 443);
-    }
+	char* gateway_resp = discord_get("https://discord.com/api/v6/gateway", discord_headers());
+	/* parse json */
+	json_t* root;
+	json_error_t error;
+	root = json_loads(gateway_resp, 0, &error);
+	free(gateway_resp);
+	json_t* url = json_string_value(json_object_get(root, "url"));
+	printf(url);
+	if (url == NULL) {
+		printf("invalid token entered, exiting...");
+		exit(0);
+	}
+	else {
+		/* pretty sure it's port 80, but whatever. */
+		discord_ws_open(url, 443);
+	}
 }
 
 void discord_ws_response() {
@@ -57,11 +59,8 @@ void discord_ws_response() {
 void discord_ws_connect() {
 
 }
-
-void discord_ws_open(char* discord_wss_url, short port) {
-
+void discord_ws_open(char* discord_wss_url, short portt) {
 }
-
 void discord_ws_close() {
 
 }
